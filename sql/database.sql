240fbb9b24478154cf0d8f046beae6eb63454590
chabaking <zaslake@gmail.com>
2023-01-18 12:10:31 +0100

    segundo commit

---
+++ sql/database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE news_portal;
+
+USE news_portal;
+
+CREATE TABLE news (
+    id_news INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(100),
+    news TEXT,
+    data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
+INSERT INTO news( title,news )
+VALUES ("primera noticia","anastasia se convierte en una superheroina que vuela y reparte canutos desde el cielo...")
